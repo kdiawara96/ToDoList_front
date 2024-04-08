f59f9c33b79a1bb1ac596baba6ba98adc6d8b108
kdiawara96 <karimdiawara96@gmail.com>
2024-04-08 01:31:46 +0000

    add color class

---
+++ lib/themes/AppColors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const ColorDeepBlue = Color(0xFF212842);
+  static const ColorGrey = Color(0xFF727677);
+  static const ColorBlue = Color(0xFF4285F4);
+
+  static const ColorBlue2 = Color(0xFF94BCFF);
+  static const ColorGreen = Color(0xFF1DCFB4);
+
+//605DEF
+  static const ColorsSpecial = Color.fromARGB(255, 95, 93, 239);
+
+  static const ColorWhite = Color(0xFFFFFFFF);
+}
